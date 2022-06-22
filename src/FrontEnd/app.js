35ebe09db5d2d8554a484d47cebe8e9ea4110605
Arthur-Morgan36 <94317959+Arthur-Morgan36@users.noreply.github.com>
2022-06-22 23:36:53 +0300

    Prettier changes

--- src/FrontEnd/app.js
+++ src/FrontEnd/app.js
@@ -2,22 +2,21 @@
 const ACTIVATION_BUTTON = document.querySelector("#party-on");
 
 if (ACTIVATION_BUTTON) {
-    ACTIVATION_BUTTON.textContent = ACTIVATION_BUTTON
-        .textContent.split(" ")
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(" ");
+  ACTIVATION_BUTTON.textContent = ACTIVATION_BUTTON.textContent
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
 function initiatePartyMode() {
-    // Do a little changes with the JS here, mostly things will be done with CSS I guess ? If not this function is here for a reason
-    console.log("Party yay!");
+  // Do a little changes with the JS here, mostly things will be done with CSS I guess ? If not this function is here for a reason
+  console.log("Party yay!");
 }
 
 document.addEventListener("keydown", (key) => {
-    if (key.code === "Space")
-        initiatePartyMode();
+  if (key.code === "Space") initiatePartyMode();
 });
 
 ACTIVATION_BUTTON?.addEventListener("click", (_) => {
-    initiatePartyMode();
+  initiatePartyMode();
 });
